f156f5ed60a4247c5156c8d0d30bc753b67df1a0
zkqiang <zkqiang@126.com>
2026-03-09 18:08:38 +0800

    更新 workflow

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,10 +1,12 @@
-import Ads from 'vue-google-adsense'
 import config from './config'
 
 export default ({ Vue }) => {
-  Vue.use(require('vue-script2'))
-  Vue.use(Ads.Adsense)
-  Vue.use(Ads.InArticleAdsense)
-  Vue.use(Ads.InFeedAdsense)
-  Vue.use(Ads.AutoAdsense, { adClient: config.themeConfig.ads.client, isNewAdsCode: true })
+  if (typeof document !== 'undefined') {
+    const Ads = require('vue-google-adsense')
+    Vue.use(require('vue-script2'))
+    Vue.use(Ads.Adsense)
+    Vue.use(Ads.InArticleAdsense)
+    Vue.use(Ads.InFeedAdsense)
+    Vue.use(Ads.AutoAdsense, { adClient: config.themeConfig.ads.client, isNewAdsCode: true })
+  }
 }
